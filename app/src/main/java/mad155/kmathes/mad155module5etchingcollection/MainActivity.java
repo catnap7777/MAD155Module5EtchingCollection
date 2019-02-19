44e341f0abb5076ba1c7b6cb8a98f6578e7e012f
kam <kmathes348@students.mchenry.edu>
2019-02-18 21:38:49 -0600

    ugh not working still2

--- app/src/main/java/mad155/kmathes/mad155module5etchingcollection/MainActivity.java
+++ app/src/main/java/mad155/kmathes/mad155module5etchingcollection/MainActivity.java
@@ -54,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
                 switch (categoryNbr) {
                     case 0: {
                         //ALL
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                    categoryFlag = true;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
 
                         break;
                     }
@@ -85,6 +100,139 @@ public class MainActivity extends AppCompatActivity {
                                 System.out.println(myMovies[row][col]);
                                 //.. col 0 is the category .. col 1 is the movie name
                                 if (col == 0 && myMovies[row][col].equalsIgnoreCase("action")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 3: {
+                        //Scifi
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("scifi")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 4: {
+                        //Comdedy
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("comedy")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 5: {
+                        //Drama
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("drama")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 6: {
+                        //Family
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("family")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 7: {
+                        //Horror
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("horror")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 8: {
+                        //Mystery/Suspense
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("mystery/suspence")) {
+                                    movieDisplayList[i] = myMovies[row][1];
+                                    i++;
+                                }
+                            }
+                        }
+                        if(categoryFlag == false) {
+                            movieDisplayList[0] = "No movies for this category";
+                        }
+
+                        break;
+                    }
+                    case 9: {
+                        //Not Categorized
+                        //.. read through whole array but build the array for output to use with listview
+                        for (int row = 0; row < myMovies.length; row++) {
+                            for (int col = 0; col < myMovies[row].length; col++) {
+                                System.out.println(myMovies[row][col]);
+                                //.. col 0 is the category .. col 1 is the movie name
+                                if (col == 0 && myMovies[row][col].equalsIgnoreCase("not categorized")) {
                                     movieDisplayList[i] = myMovies[row][1];
                                     i++;
                                 }
